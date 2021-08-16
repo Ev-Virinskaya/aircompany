6cae75299b2309641f675146697bb06218e563b7
Virinskaya <e.virinskaya@gmail.com>
2021-08-16 21:14:07 +0300

    Fixed code

--- Java/src/main/java/planes/ExperimentalPlane.java
+++ Java/src/main/java/planes/ExperimentalPlane.java
@@ -1,24 +1,24 @@
-package Planes;
+package planes;
 
 import models.ClassificationLevel;
 import models.ExperimentalTypes;
 
-public class experimentalPlane extends Plane{
+public class ExperimentalPlane extends Plane{
 
-    private ExperimentalTypes type;
+    private ExperimentalTypes typeOfPlane;
     private ClassificationLevel classificationLevel;
 
-    public experimentalPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, ExperimentalTypes type, ClassificationLevel classificationLevel) {
+    public ExperimentalPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, ExperimentalTypes typeOfPlane, ClassificationLevel classificationLevel) {
         super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
-        this.type = type;
+        this.typeOfPlane = typeOfPlane;
         this.classificationLevel = classificationLevel;
     }
 
-    public ClassificationLevel getClassificationLevel(){
+    public ClassificationLevel getClassificationLevel() {
         return classificationLevel;
     }
 
-    public void setClassificationLevel(ClassificationLevel classificationLevel){
+    public void setClassificationLevel(ClassificationLevel classificationLevel) {
         this.classificationLevel = classificationLevel;
     }
 
@@ -35,7 +35,7 @@ public class experimentalPlane extends Plane{
     @Override
     public String toString() {
         return "experimentalPlane{" +
-                "model='" + model + '\'' +
+                "model='" + getModel() + '\'' +
                 '}';
     }
 }
---
+++ Java/src/main/java/planes/MilitaryPlane.java
@@ -0,0 +1,46 @@
+package planes;
+
+import models.MilitaryType;
+
+import java.util.Objects;
+
+public class MilitaryPlane extends Plane{
+
+    private MilitaryType typeOfPlane;
+
+    public MilitaryPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, MilitaryType typeOfPlane) {
+        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
+        this.typeOfPlane = typeOfPlane;
+    }
+
+    public MilitaryType getTypeOfPlane() {
+        return typeOfPlane;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString().replace("}",
+                ", type=" + typeOfPlane +
+                        '}');
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof MilitaryPlane)) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+        MilitaryPlane that = (MilitaryPlane) o;
+        return typeOfPlane == that.typeOfPlane;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), typeOfPlane);
+    }
+}
